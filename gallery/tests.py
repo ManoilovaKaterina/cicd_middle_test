d4fa41e8b3dc269bc32e69e35fd9c676a52888ff
ManoilovaKaterina <katya.manoylova@gmail.com>
2023-05-23 13:37:15 +0300

    tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,17 @@
+import pytest
 from django.test import TestCase
-
+from django.urls import reverse
+from gallery.models import Category, Image
+from gallery.views import gallery_view, image_detail
 # Create your tests here.
+
+def test_gallery_view(client):
+    url = reverse('main')
+    response = client.get(url)
+    assert response.status_code == 200
+
+def test_image_detail_view(client):
+    image = Image.objects.create(title='Test Image', category=Category, image_file='test.jpg')
+    url = reverse('image_detail', kwargs={'pk': image.pk})
+    response = client.get(url)
+    assert response.status_code == 200
